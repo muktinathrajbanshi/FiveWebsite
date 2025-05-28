67e43bff03a611f85f90d0cb13b0f21f17b585f7
unknown <muktinathrajbanshi786@gmail.com>
2025-05-28 21:32:36 +0545

    about section starts

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Flamenco:wght@300;400&family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Jost:ital,wght@0,100..900;1,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Piazzolla:ital,opsz,wght@0,8..30,100..900;1,8..30,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
@@ -80,6 +83,19 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+
+<section class="my-5">
+    <div class= "py-5">
+        <h2 class="text-center">About Us</h2>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+                <img src="images/cc5.png" class="img-fluid" alt="">
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
